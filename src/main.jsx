d3249b311504f2ebd362c006282f988e7fe56db3
sanjay8vsk <sanjay8vsk@gmail.com>
2026-03-10 23:39:09 +0100

    updated code

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import App from "./App";
+import GlobalMap from "./pages/GlobalMap";
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/map" element={<GlobalMap />} />
+    </Routes>
+  </BrowserRouter>
+
+);
